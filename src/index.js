1e5e83641effd22a867b67289f234d19019e7524
Saurabh <raisaurabh265@gmail.com>
2022-08-07 00:39:45 +0530

    fixed overflow hidden issue fixed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,67 @@
-const transitionStyles = {
-  transform: `translateY(-${currentFloor * 174}px)`,
-  transitionDuration: `${(currentFloor - prevFloorCount) * 2}s`,
-};
-const floors = [
-  { downButton: "Down", floor: "F-10", id: 10 },
-  { upButton: "UP", downButton: "Down", floor: "F-9", id: 9 },
-  { upButton: "UP", downButton: "Down", floor: "F-8", id: 8 },
-  { upButton: "UP", downButton: "Down", floor: "F-7", id: 7 },
-  { upButton: "UP", downButton: "Down", floor: "F-6", id: 6 },
-  { upButton: "UP", downButton: "Down", floor: "F-5", id: 5 },
-  { upButton: "UP", downButton: "Down", floor: "F-4", id: 4 },
-  { upButton: "UP", downButton: "Down", floor: "F-3", id: 3 },
-  { upButton: "UP", downButton: "Down", floor: "F-2", id: 2 },
-  { upButton: "UP", downButton: "Down", floor: "F-1", id: 1 },
-];
+const floor_input = document.getElementById("floor_input");
+const lift_input = document.getElementById("lift_input");
+const generate_lift_button = document.getElementById("generate_lift_bttn");
+const generate_floor_button = document.getElementById("generate_floor_bttn");
+const buttons = document.getElementsByClassName("bttn");
+const lift = document.getElementsByClassName("lift");
+const floorInput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const liftInput = [1];
+const currentFloor = "";
+// const transitionStyles = {
+//   transform: `translateY(-${currentFloor * 174}px)`,
+//   transitionDuration: `${(currentFloor - prevFloorCount) * 2}s`,
+// };
+
+const floorSection = document.createElement("floor_section");
+floorSection.className = "floor_section";
+
+const floor = document.createElement("div");
+floor.className = "floors";
+
+// const lift = document.createElement("div");
+// lift.className = "lift";
+
+const lift_left_door = document.createElement("div");
+lift_left_door.className = "lift_left_door";
+
+const lift_right_door = document.createElement("div");
+lift_right_door.className = "lift_right_door";
+
+const upButton = document.createElement("button");
+upButton.className = "bttn";
+
+const downButton = document.createElement("button");
+downButton.className = "bttn";
+
+// lift.appendChild(lift_left_door, lift_right_door);
+
+generate_floor_button.addEventListener("click", () => {
+  if (floor_input.value === "") {
+    alert("Please enter a  number");
+  } else {
+    const numberOfFloors = floor_input.value;
+    const floorArr = Array.from(
+      { length: numberOfFloors },
+      (_, index) => index + 1
+    );
+    for (let i = 0; i < floorArr.length; i++) {
+      floor.appendChild(lift, upButton, downButton);
+    }
+    floorSection.appendChild(floor);
+    console.log(floorArr);
+    console.log(`generate floor button clicked with ${floor_input.value}`);
+  }
+});
+
+generate_lift_button.addEventListener("click", () => {
+  console.log("generate lift button clicked");
+});
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", (e) => {
+    const currentButton = e.target.id;
+    lift.style.transform = `translateY(-${currentButton * 174}px)`;
+    // lift.style.transitionDuration = `${(currentButton - currentFloor) * 2}s`;
+    console.log("button clicked");
+  });
+}
