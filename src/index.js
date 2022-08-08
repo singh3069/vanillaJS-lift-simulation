df0cafb7ecd0f78059fd4f45865e445dc9113617
Saurabh <raisaurabh265@gmail.com>
2022-08-08 17:19:28 +0530

    lift is moving 2s per floor and doors are opening and closing at 2.5s after the movement

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ const generate_lift_button = document.getElementById("generate_lift_btn");
 const generate_floor_button = document.getElementById("generate_floor_btn");
 const buttons = document.getElementsByClassName("btn");
 const lift = document.getElementsByClassName("lift");
+const lift_left_door = document.getElementsByClassName("liftLeftDoor");
+const lift_right_door = document.getElementsByClassName("liftRightDoor");
 const floorInput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const liftInput = [1];
 const currentFloor = "";
+var prevFloorCount = 0;
+var nextFloorCount = 0;
 // const transitionStyles = {
 //   transform: `translateY(-${currentFloor * 174}px)`,
 //   transitionDuration: `${(currentFloor - prevFloorCount) * 2}s`,
@@ -57,15 +61,45 @@ const currentFloor = "";
 //   console.log("generate lift button clicked");
 // });
 
+const doorsTransition = () => {
+  const doorOpen = () => {
+    lift_left_door[0].style.cssText = `
+    width: 0%;
+    transition: width 2.5s;
+    `;
+    lift_right_door[0].style.cssText = `
+  width: 0%;
+  transition: width 2.5s;
+  `;
+  };
+  const doorClose = () => {
+    lift_left_door[0].style.cssText = `
+    width: 50%;
+    transition: width 2.5s ;
+    `;
+    lift_right_door[0].style.cssText = `
+  width: 50%;
+  transition: width 2.5s;
+  `;
+  };
+  doorOpen();
+  // so that door  close animation starts  after 2.5 seconds
+  setTimeout(doorClose, 2500);
+};
+
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", (e) => {
     const parentID = parseInt(e.target.parentElement.id);
-    // console.log(`button clicked with ID ${parentID}`);
-    // console.log(lift);
-    // lift.style.height = "100px";
-    lift[0].style.transform = `translateY(-${parentID * 174}px)`;
-    lift[0].style.transitionDuration = "2s";
-
-    // lift[0].style.transitionDuration = `${(currentButton - currentFloor) * 2}s`;
+    nextFloorCount = parentID;
+    const distanceToCoverByLift = parentID * 180;
+    setTimeout(() => {
+      prevFloorCount = nextFloorCount;
+    }, 50);
+    const liftSpeed = Math.abs(nextFloorCount - prevFloorCount);
+    lift[0].style.transform = `translateY(-${distanceToCoverByLift}px)`;
+    lift[0].style.transitionDuration = `${liftSpeed * 2}s`;
+    setTimeout(() => {
+      doorsTransition();
+    }, liftSpeed * 2 * 1000);
   });
 }
